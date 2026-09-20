932ce5afbd0ea1f7070501710db602808c49e4e7
Harlem Kirua <debabel1hackamani@gmail.com>
2026-09-20 10:21:21 +0000

    test: update expected root response

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -16,11 +16,8 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('should return "Apprendre le CI/CD Jenkins"', () => {
+    expect(appController.getHello()).toBe('Apprendre le CI/CD Jenkins');
   });
 
   afterEach(async () => {
