aee9cc5cdfac359bb36ea492e50b4f8b370613c6
Harlem Kirua <debabel1hackamani@gmail.com>
2026-09-20 10:14:43 +0000

    test: trigger Jenkins webhook

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Apprendre le CI/CD avec jenkins';
+    return 'Apprendre le CI/CD Jenkins';
   }
 }
